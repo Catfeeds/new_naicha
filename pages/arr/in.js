f55e306da743db832b753d5156dcbebe3105ef74
cqingt <784966200@qq.com>
2018-09-21 00:04:12 +0800

    order

--- pages/arr/in.js
+++ pages/arr/in.js
@@ -82,24 +82,6 @@ Page({
         icon: '/images/process1.png'
       }
     ],
-    progress: [
-      {
-        word: "无糖",
-        state: 1
-      }, {
-        word: "少糖",
-        state: 0
-      }, {
-        word: "单糖",
-        state: 0
-      }, {
-        word: "正常",
-        state: 0
-      }, {
-        word: "多糖",
-        state: 0
-      }
-    ],
 
 // 购物车
     cups:[
@@ -117,42 +99,7 @@ Page({
           num: 12,
           cal: 100,
           price: 100.00
-        },
-        {
-          list: 3,
-          name: "回肝普洱",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
-        {
-          list: 4,
-          name: "回肝普洱",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
-        {
-          list: 5,
-          name: "回肝普洱",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
-        {
-          list: 6,
-          name: "回肝普洱",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
-        {
-          list: 7,
-          name: "春菊",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
+        }
       ],
       [
         {
@@ -161,49 +108,7 @@ Page({
           num: 12,
           cal: 100,
           price: 100.00
-        },
-        {
-          list: 2,
-          name: "回肝普洱",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
-        {
-          list: 3,
-          name: "回肝普洱",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
-        {
-          list: 4,
-          name: "回肝普洱",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
-        {
-          list: 5,
-          name: "回肝普洱",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
-        {
-          list: 6,
-          name: "回肝普洱",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
-        {
-          list: 7,
-          name: "春菊",
-          num: 12,
-          cal: 100,
-          price: 100.00
-        },
+        }
       ]
     ],
     cup: [
@@ -211,90 +116,29 @@ Page({
           list: 1,
           name: "回肝普洱",
           num: 12,
-        calorie: 100,
-          price: 100.00
-        },
-        {
-          list: 2,
-          name: "回肝普洱",
-          num: 12,
           calorie: 100,
           price: 100.00
-        },
-        {
-          list: 3,
-          name: "回肝普洱",
-          num: 12,
-          calorie: 100,
-          price: 100.00
-        },
-        {
-          list: 4,
-          name: "回肝普洱",
-          num: 12,
-          calorie: 100,
-          price: 100.00
-        },
-        {
-          list: 5,
-          name: "回肝普洱",
-          num: 12,
-          calorie: 100,
-          price: 100.00
-        },
-        {
-          list: 6,
-          name: "回肝普洱",
-          num: 12,
-          calorie: 100,
-          price: 100.00
-        },
-        {
-          list: 7,
-          name: "春菊",
-          num: 12,
-          calorie: 100,
-          price: 100.00
-        },
+        }
       ],
-    // 多选数据
-    selects:[
-      {
-        name:"薄荷叶",
-        id:0,
-        bg: "#D6D3D3",
-        color: "black"
-      },{
-        name:"果醋",
-        id:1,
-        bg: "#D6D3D3",
-        color: "black"
-      },{
-        name:"配茶",
-        id:2,
-        bg: "#D6D3D3",
-        color:"black"
-      }
-    ],
-  //  冰量 选择
+    //  冰量 选择
     ices:[
       {
         name:"正常冰",
-        bgNol:"#fff",
-        colNol:"black",
-        id:0
-      },
-      {
-        name:"少冰",
-        bgNol: "#fff",
-        colNol: "black",
+        bgColor:"#fff",
+        color:"black",
         id:1
       },
       {
-        name:"去冰",
-        bgNol: "#fff",
-        colNol: "black",
+        name:"少冰",
+        bgColor: "#fff",
+        color: "black",
         id:2
+      },
+      {
+        name:"去冰",
+        bgColor: "#fff",
+        color: "black",
+        id:3
       }
     ],
     bgHot:"#fff",
@@ -310,20 +154,13 @@ Page({
     show: 3,//同时显示的数量
     cur: 0,//当前高亮的元素
     secondShow: 3,
-    secondCur: 1,
     option: 3,
-    options: 0,
-    classFour:3,
-    classFours:0,
-    classFive: 3,
-    classFives: 1,
     iceFlase:false,
     cold:false,
     hot: true,
     bgProcess:"#666",
     bgScon:"#666",
     bgThird:"#666",
-    
     cupMenu:true,
     sub:true,
     payOrder:true
@@ -369,102 +206,37 @@ Page({
   iceBtn:function(e){
     var that =this;
     var iceId = e.currentTarget.dataset.id;
-    if (iceId == 0) {
-      that.data.ices[0].bgNol = "#000";
-      that.data.ices[0].colNol = "#fff";
-      that.data.ices[1].bgNol = "#fff";
-      that.data.ices[1].colNol = "#000";
-      that.data.ices[2].bgNol = "#fff";
-      that.data.ices[2].colNol = "#000";
-      console.log(0);
-    } else if (iceId == 1) {
-      that.data.ices[1].bgNol = "#000";
-      that.data.ices[1].colNol = "#fff";
-      that.data.ices[0].bgNol = "#fff";
-      that.data.ices[0].colNol = "#000";
-      that.data.ices[2].bgNol = "#fff";
-      that.data.ices[2].colNol = "#000";
-      console.log(1);
-    } else {
-      that.data.ices[2].bgNol = "#000";
-      that.data.ices[2].colNol = "#fff";
-      that.data.ices[0].bgNol = "#fff";
-      that.data.ices[0].colNol = "#000";
-      that.data.ices[1].bgNol = "#fff";
-      that.data.ices[1].colNol = "#000";
-      console.log(2);
+    var ices = that.data.ices;
+
+    for (var item of ices) {
+      if (item['id'] == iceId) {
+        item['bgColor'] = '#000';
+        item['color'] = '#fff';
+      } else {
+        item['bgColor'] = '#fff';
+        item['color'] = '#000';
+      }
     }
+   
     this.setData({
-      ices:that.data.ices
+      ices: ices
+    })
+  },
+  // 冷热选择
+  chooseTemp: function(e) {
+    var that = this;
+    var cold = e.currentTarget.dataset.type == 'cold';
+    
+    this.setData({
+      bgHot: cold ? '#fff' : '#000',
+      colHot: cold ? '#000' : '#fff',
+      bgCold: cold ? '#000' : '#fff',
+      colCold: cold ? '#fff' : '#000',
+      points: ! cold,
+      choises: ! cold
     })
   },
 
-  hotBtn:function(e){
-    var that = this;
-    that.data.bgHot="#000";
-    that.data.colHot="#fff";
-    that.data.bgCold="#fff";
-    that.data.colCold="#000";
-    that.data.points = true;
-    that.data.choises = true;
-    this.setData({
-      bgHot: that.data.bgHot,
-      colHot: that.data.colHot,
-      bgCold: that.data.bgCold,
-      colCold: that.data.colCold,
-      points: that.data.points,
-      choises:that.data.choises
-    })
-  },
-  coldBtn:function(e){
-    var that = this;
-    that.data.bgHot = "#fff";
-    that.data.colHot = "#000";
-    that.data.bgCold = "#000";
-    that.data.colCold = "#fff";
-    that.data.points = false;
-    that.data.choises = false;
-    this.setData({
-      bgHot: that.data.bgHot,
-      colHot: that.data.colHot,
-      bgCold: that.data.bgCold,
-      colCold: that.data.colCold,
-      points: that.data.points,
-      choises: that.data.choises
-    })
-  },
-  // 多选
-  sel:function(e){
-    var that = this;
-    var setId = e.currentTarget.dataset.id;
-    for(var i = 0; i<that.data.selects.length;i++){
-      if ( setId == 0 ){
-        that.data.selects[0].bg="#000";
-        that.data.selects[0].color="#fff";
-        that.data.selects[1].bg = "#D6D3D3";
-        that.data.selects[1].color = "#000";
-        that.data.selects[2].bg = "#D6D3D3";
-        that.data.selects[2].color = "#000";
-      } else if (setId == 1){
-        that.data.selects[1].bg = "#000";
-        that.data.selects[1].color = "#fff";
-        that.data.selects[0].bg = "#D6D3D3";
-        that.data.selects[0].color = "#000";
-        that.data.selects[2].bg = "#D6D3D3";
-        that.data.selects[2].color = "#000";
-      }else{
-        that.data.selects[2].bg = "#000";
-        that.data.selects[2].color = "#fff";
-        that.data.selects[0].bg = "#D6D3D3";
-        that.data.selects[0].color = "#000";
-        that.data.selects[1].bg = "#D6D3D3";
-        that.data.selects[1].color = "#000";
-      }
-    };
-    this.setData({
-      selects:that.data.selects
-    })
-  },
   // 选项卡
   navbarTab: function (e) {
     this.setData({
@@ -501,31 +273,6 @@ Page({
     that.setData(data);
 
     that.calculate(); // 购物车计算
-  },
-  change(e) {
-    this.setData({
-      cur: e.detail.current
-    });
-  },
-  secondLevel(e){
-    this.setData({
-      secondCur: e.detail.current
-    });
-  },
-  optionChange(e) {
-    this.setData({
-      options: e.detail.current
-    });
-  },
-  optionFour(e) {
-    this.setData({
-      classFours: e.detail.current
-    });
-  },
-  optionFive(e) {
-    this.setData({
-      classFives: e.detail.current
-    });
   },
   
   /**
@@ -698,14 +445,17 @@ Page({
     var that = this;
     var thirdGoods = that.data.thirdGoods;
     var pk = e.currentTarget.dataset.id;
+    var sugarId = that.data.sugarId;
+    var init = that.data.processData[0];
 
     for (var item of thirdGoods) {
 
       if (item['pk'] == pk) {
         item['bgProcess'] = '#000';
-        
+        item['processData'] = that.data.processData;
       } else {
         item['bgProcess'] = '#666';
+        item['processData'] = that.data.processData;
       }
     }
 
@@ -721,89 +471,61 @@ Page({
     var thirdGoods = that.data.thirdGoods;
     var pk = e.currentTarget.dataset.id;
     var weight = e.currentTarget.dataset.weight;
+    var selected = true;  // 选中
 
     for (var i = 0, l = thirdGoods.length; i < l; i++) {
-      //for (var item of thirdGoods) {
-      console.log(thirdGoods[i]);
-      if (thirdGoods[i]['pk'] == pk) {
+        let parent = [];
         for (var j = 0; j < thirdGoods[i]['processData'].length; j++) {
+          let item = {};
 
-        //for (var weight of item['processData']) {
-          //console.log(item['processData'][j]);
-          console.log(thirdGoods[i]['processData'][j]);
-          if (thirdGoods[i]['processData'][j]['name'] == weight) {
-            thirdGoods[i]['processData'][j]['start'] = '#000';
-            thirdGoods[i]['processData'][j]['end'] = '#000';
-            thirdGoods[i]['processData'][j]['icon'] = '/images/process2.png';
+          if (thirdGoods[i]['pk'] == pk ) {
+
+            if (true == selected) {
+              item.name = thirdGoods[i]['processData'][j]['name'];
+              item.start = j == 0 ? '#fff' : '#000'; // 第一个
+              item.end = j == 4 ? '#fff' :'#000';    // 最后一个
+              item.icon = '/images/process2.png';
+            } else {
+              item.name = thirdGoods[i]['processData'][j]['name'];
+              item.start = j == 0 ? '#fff' : '#666';
+              item.end = j == 4 ? '#fff' : '#666';
+              item.icon = '/images/process1.png';
+            }
+
+            if (thirdGoods[i]['processData'][j]['name'] == weight) {
+              selected = false;
+            }
+          } else { // 非当前 糖类的，保持原样
+            item.name = thirdGoods[i]['processData'][j]['name'];
+            item.start = j == 0 ? '#fff' : '#666';
+            item.end = j == 4 ? '#fff' :'#666';
+            item.icon = '/images/process1.png';
           }
+          parent.push(item);
         }
+
+        thirdGoods[i]['processData'] = parent;
+    }
+
+    // 选中糖类
+    for (var item of thirdGoods) {
+
+      if (item['pk'] == pk) {
+        item['bgProcess'] = '#000';
+
+      } else {
+        item['bgProcess'] = '#666';
       }
     }
-    console.log(thirdGoods);
-    
+
     that.setData({
+      sugarId: pk,
       sugarWeight: weight,
       thirdGoods: thirdGoods
     }); 
-
-    return;
-
-    let index = e.target.dataset.index
-    let mypro = this.data.progress
-
-    if (index == 0) {
-      for (var i = 1; i < mypro.length; i++) {
-        var item = mypro[i]
-        item.state = 0
-      }
-    } else {
-      for (var i = 1; i < index + 1; i++) {
-        var item = mypro[i]
-        item.state = 1
-      }
-      for (var i = index + 1; i < mypro.length; i++) {
-        var item = mypro[i]
-        item.state = 0
-      }
-    }
-    console.log(this.data.sugarId +"这是sugarId")
-    if (this.data.sugarId != 1) {
-      return;
-    }
-    this.setData({
-      progress: mypro
-    })
-    this.setPeocessIcon();
   },
 
-  setPeocessIcon: function () {
-    var index = 0 //记录状态为1的最后的位置
-    var processArr = this.data.processData
-
-    for (var i = 0; i < this.data.progress.length; i++) {
-      var item = this.data.progress[i]
-      processArr[i].name = item.word
-      if (item.state == 1) {
-        index = i
-        processArr[i].icon = "/images/process3.png"
-        processArr[i].start = "#000"
-        processArr[i].end = "#000"
-      } else {
-        processArr[i].icon = "/images/process1.png"
-        processArr[i].start = "#aaa"
-        processArr[i].end = "#aaa"
-      }
-    }
-    processArr[index].icon = "/images/process2.png"
-    processArr[index].end = "#aaa"
-    processArr[0].start = "#fff"
-    processArr[this.data.progress.length - 1].end = "#fff";
-
-    this.setData({
-      processData: processArr
-    })
-  },
-
+ // 浮点数相加
   floatAdd: function (arg1, arg2){
     var r1, r2, m;
     try{ 
@@ -831,6 +553,7 @@ Page({
       // 多选
       if (Array.isArray(carts[k])) {
         for (let item of carts[k]) {
+          item['num'] = 1;
           cup.push(item);
           nums++;
         }
@@ -904,6 +627,5 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
   }
 })
